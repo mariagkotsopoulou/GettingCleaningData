35512f2c31eff4e82a132ff3db741e9eb8f21574
mariagk <maria.gotsopoulou@hotmail.com>
2016-09-24 12:49:09 +0200

    add codebook readme run_analysis tidycsv file

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# GettingCleaningData
-Coursera Getting and Cleaning Data Course Project
+# Coursera Getting and Cleaning Data Course Project
+
+
+## Data Set Information:
+The dataset used is the Human Activity Recognition Using Smartphones Data Set from 
+the UCI Machine Learning Repository. 
+http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+
+
+## Files included:
+
+####1.`CodeBook.Rmd`: 
+This is an explanatory file of the data used and describes the objective of the code `run_analysis.R`.
+In addition, it outlines all steps performed in the code such as data sources read, data manipulation executed
+and data output generated.
+
+####2.`run_analysis.R`:
+This code uses as a data input the above mentioned UCI dataset to generate a tidy data set saved as `UCIHARTidyData.csv`. 
+
+
+####3.`UCIHARTidyData.csv`:
+This csv file is the output of the code `run_analysis.R`. It is a tidy data set to be used for further future analysis. 
--- run_analysis.R
+++ run_analysis.R
@@ -1,33 +1,109 @@
 
-# 2.Extracts only the measurements on the mean and standard deviation for each measurement.
-# 3.Uses descriptive activity names to name the activities in the data set
-# 4.Appropriately labels the data set with descriptive variable names.
-# 5.From the data set in step 4, creates a second, independent tidy data set 
-# with the average of each variable for each activity and each subject.
+#### LOAD LIBRARIES ########################################################
+library(dplyr)
+library(data.table) 
 
-#30 volunteers 
-# each person performed six activities 
-# (WALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING)
 
-# we captured 3-axial linear acceleration and 3-axial angular velocity 
-# accelerometer and gyroscope 3-axial raw signals tAcc-XYZ and tGyro-XYZ  -->tBodyGyro-XYZ
-# the acceleration signal was then separated into 
-# body and gravity acceleration signals (tBodyAcc-XYZ and tGravityAcc-XYZ) 
+#### 1.MERGE TRAINING & TEST ##############################################
 
-# 1.Merges the training and the test sets to create one data set.
+## download the data 
+fileUrl<- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl,destfile="./UCIHARDataset.zip", method="libcurl")
+## extract the zip file 
+unzip("./UCIHARDataset.zip", exdir = "./")
 
+## load the training data set
 train <- read.table("UCI HAR Dataset/train/X_train.txt")
-
-NamesFeatures <- read.table("UCI HAR Dataset/features.txt")
-
-colnames(train) <- as.character(NamesFeatures$V2)
-
+## load the variable names 
+namesfeatures <- read.table("UCI HAR Dataset/features.txt")
+## add the variable names to the training data set 
+colnames(train) <- as.character(namesfeatures$V2)
+## each row of the data set corresponds to measurements of an activity
 trainlabels <- read.table("UCI HAR Dataset/train/y_train.txt")
-
+## each row of the data set corresponds to measurements of a person performing activity
 subjecttrain <- read.table("UCI HAR Dataset/train/subject_train.txt")
-
+## add to the dataset the person (i.e. subject) and activity variable row ids 
 train <- cbind(subjecttrain, trainlabels, train)
+colnames(train)[1:2] <- c("Subject","Activity")
 
-colnames(train)[1:2] <- c("subject","activity")
+## load the test data set
+test <- read.table("UCI HAR Dataset/test/X_test.txt")
+## add the variable names to the testing data set 
+colnames(test) <- as.character(namesfeatures$V2)
+## each row of the data set corresponds to measurements of an activity
+testlabels <- read.table("UCI HAR Dataset/test/y_test.txt")
+## each row of the data set corresponds to measurements of a person peforming activity
+subjecttest <- read.table("UCI HAR Dataset/test/subject_test.txt")
+## add to the dataset the person (i.e. subject) and activity variable row ids 
+test <- cbind(subjecttest, testlabels, test)
+colnames(test)[1:2] <- c("Subject","Activity")
 
-head(train[,1:5])
+## combine training and test dataset
+data <- rbind(train, test)
+
+## remove objects not to be used further on
+rm(subjecttest, subjecttrain, test, testlabels, train, trainlabels)
+
+#### 2.GET MEAN AND STD FOR EACH MEASUREMENT ######################
+
+## find variable names containing mean and standard deviation
+## only interested in the measurement variables 
+## and not the variables created a posteriori i.e. Jerk signals, signal magnitude and Fast Fourier Transform (FFT) 
+subsetnamesfeatures <- namesfeatures %>%
+                        filter(grepl("*-mean()|*-std()", V2) & !grepl("*Jerk*|*Mag*|^f", V2))
+
+## select only the columns with the variables of interest
+## add second filter so as to not select the columns containing numbers as we are only interested in the X,Y,Z ones
+subsetdatanames <- c("Subject","Activity", as.vector(subsetnamesfeatures$V2))
+data <- data[,!grepl('[[:digit:]]', colnames(data))]  %>% select(one_of(subsetdatanames))
+
+## remove objects not to be used further on
+rm(subsetdatanames,subsetnamesfeatures, namesfeatures)
+
+#### 3. ADD DESCRIPTIVE ACTIVITY NAMES  ######################################
+
+# load the activity labels (WALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING)
+activitylabels <- read.table("UCI HAR Dataset/activity_labels.txt")
+
+# recode the activity column using the activity labels 
+data <- data %>% 
+          mutate(Activity = plyr::mapvalues(Activity, as.vector(activitylabels$V1),
+                                            as.vector(tolower(activitylabels$V2)))) 
+
+## remove objects not to be used further on
+rm(activitylabels)
+
+#### 4.ADD DESCRIPTIVE LABELS VARIABLE NAMES. ##########################################
+
+# tBodyAcc  --> sensor acceleration signal , body motion component
+# tGravityAcc  -->   sensor acceleration signal , gravitational component  
+# tBodyGyro --> Triaxial Angular velocity from the gyroscope.
+
+# Across all columns, replace all instances of "..." with "..."
+names(data) <- gsub("tBodyAcc", "BodyMotionAcceleration", names(data))
+names(data) <- gsub("tGravityAcc", "GravitationalAcceleration", names(data))
+names(data) <- gsub("tBodyGyro", "GyroscopeVelocity", names(data))
+
+
+#### 5.CREATE TIDY DATA WITH AVERAGE BY ACTIVITY & SUBJECT. ######################
+
+## transform data from wide to long format by activity and subject
+datamelt = melt(setDT(data), id = c("Activity","Subject" ))
+## separate the variable by its components 
+## Variable: BodyMotionAcceleration or GravitationalAcceleration or GyroscopeVelocity
+## Measuremnt: mean or std (standard deviation)
+## Triaxial: X, Y or Z 
+datamelt[, c("Variable", "Measurement","Triaxial") := tstrsplit(variable, "-", fixed=TRUE)]
+## remove the parenthesis string from mean() and std()
+datamelt[, Measurement := gsub("()", "", Measurement,fixed="TRUE")]  
+## change column types 
+cols <- c("Activity","Subject","Measurement","Triaxial" )
+datamelt[, (cols) := lapply(.SD, factor), .SDcols=cols]
+
+## create tidy data set of the variable average by activity and subject 
+datacast = dcast(datamelt, Activity +Subject  + Measurement + Triaxial  ~ Variable , value.var = "value", fun=mean)
+str(datacast)
+#### SAVE TIDY DATA SET TO BE USED FOR LATER ANALYSIS
+
+write.csv(datacast, "UCIHARTidyData.csv")
+
